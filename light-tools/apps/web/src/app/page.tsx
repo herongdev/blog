b59a3f04e9dd0f149f375618317f2b261419d05c
何荣 <379925144@qq.com>
2026-06-12 23:44:27 +0800

    Fix light tools root redirect

--- light-tools/apps/web/src/app/page.tsx
+++ light-tools/apps/web/src/app/page.tsx
@@ -1,6 +1,5 @@
 import { redirect } from "next/navigation";
-import { withBasePath } from "@/lib/base-path";
 
 export default function HomePage() {
-  redirect(withBasePath("/tools"));
+  redirect("/tools");
 }
